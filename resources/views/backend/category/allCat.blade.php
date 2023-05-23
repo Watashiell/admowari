ae2bc98e9e283d50ab5f7a27ea0a8625efd0755a
Watashiell <dzellyas@gmail.com>
2023-05-23 23:32:33 +0700

    progress
    part1

--- resources/views/backend/category/allCat.blade.php
+++ resources/views/backend/category/allCat.blade.php
@@ -1,111 +1,276 @@
 @extends('backend/layouts.template')
 
 @section('content')
-    <section id="main-content">
-        <section class="wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h3 class="page-header"><i class="icon_document_alt"></i> Riwayat Hidup</h3>
-                    <ol class="breadcrumb">
-                        <li><i class="fa fa-home"></i><a href="{{ url('dashboard') }}">Home</a></li>
-                        <li><i class="icon_document_alt"></i>Riwayat Hidup</li>
-                        <li><i class="fa fa-files-o"></i>Pendidikan</li>
-                    </ol>
-                </div>
-            </div>
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <section class="panel">
-                        <header class="panel-heading">
-                            {{ isset($admin_lecturer) ? 'Mengubah' : 'Menambahkan' }} Pendidikan
-                        </header>
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input. <br><br>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <title>owari | Category</title>
+    <link href="{{ asset('backend/css/lib/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('backend/css/helper.css')}}" rel="stylesheet">
+    <link href="{{ asset('backend/css/style.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body class="fix-header fix-sidebar">
+
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+        </svg>
+    </div>
+
+    <div id="main-wrapper">
+
+        <div class="header">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="dashboard.html">
+
+                        <span><img src="{{ asset('backend/images/lo.png')}}" alt="homepage" class="dark-logo" /></span>
+                    </a>
+                </div>
+                <div class="navbar-collapse">
+
+                    <ul class="navbar-nav mr-auto mt-md-0">
+
+
+
+
+                    </ul>
+
+                    <ul class="navbar-nav my-lg-0">
+
+
+
+                        <li class="nav-item dropdown">
+
+                            <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
                                 <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
+                                    <li>
+                                        <div class="drop-title">Notifications</div>
+                                    </li>
+
+                                    <li>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all
+                                                notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                    </li>
                                 </ul>
                             </div>
-                        @endif
-                        <div class="panel-body">
-                            <div class="form">
-                                <form class="form-validate form-horizontal" id="pendidikan_form" method="POST" 
-                                action="{{ isset($pendidikan) ? route('pendidikan.update',$pendidikan->id) : route('pendidikan.store') }}">
-                                {!! csrf_field() !!}
-                                {!! isset($pendidikan) ? method_field('PUT') : '' !!}
-                                <div class="form-group">
-                                    <label for="cname" class="control-label col-lg-2">Nama Sekolah <span
-                                        class="required">*</span></label>
-                                        <div class="col-lg-10">
-                                            <input class="form-control" id="nama" name="nama" minlength="5" type="text" value="{{ isset($pendidikan) ? $pendidikan->nama : '' }}"
-                                            required>
-                                        </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false"><img
+                                    src="{{ asset('backend/images/bookingSystem/user-icn.png')}}" alt="user"
+                                    class="profile-pic" /></a>
+                            <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                                <ul class="dropdown-user">
+
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        <div class="left-sidebar">
+
+            <div class="scroll-sidebar">
+
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li class="nav-label">Home</li>
+                        <li> <a href="dashboard.html"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
+                        <li class="nav-label">Log</li>
+                        <li> <a href="users.html"> <span><i class="fa fa-user f-s-20 "></i></span><span>Users</span></a>
+                        </li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
+                                    class="fa fa-archive f-s-20 color-warning"></i><span
+                                    class="hide-menu">Category</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="all_category.html">All Category</a></li>
+                                <li><a href="add_tag.html">Add Tag</a></li>
+                                <li><a href="add_category.html">Add Category</a></li>
+
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fas fa-tshirt"
+                                    aria-hidden="true"></i><span class="hide-menu">Product</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="all_product.html">All Product</a></li>
+                                <li><a href="add_product.html">Add Product</a></li>
+
+
+                            </ul>
+                        </li>
+                        <li> <a href="all_orders.html"><i class="fa fa-shopping-cart"
+                                    aria-hidden="true"></i><span>Orders</span></a></li>
+
+                    </ul>
+                </nav>
+
+            </div>
+
+        </div>
+
+        <div class="page-wrapper">
+
+
+
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-12">
+
+
+
+
+
+
+                        <div class="col-lg-12">
+                            <div class="card card-outline-primary">
+                                <div class="card-header">
+                                    <h4 class="m-b-0 text-white">All Category</h4>
                                 </div>
-                                <div class="form-group">
-                                    <label for="email" class="control-label col-lg-2">Tingkatan <span
-                                        class="required">*</span></label>
-                                        <div class="col-lg-10">
-                                            <select class="form-control m-bot15" name="tingkatan" id="tingkatan" required>
-                                                <option value="1 {{ (isset($pendidikan) && $pendidikan->tingkatan == 1) ? 'selected' : '' }}">TK</option>
-                                                <option value="2 {{ (isset($pendidikan) && $pendidikan->tingkatan == 2) ? 'selected' : '' }}">SD</option>
-                                                <option value="3 {{ (isset($pendidikan) && $pendidikan->tingkatan == 3) ? 'selected' : '' }}">SMP</option>
-                                                <option value="4 {{ (isset($pendidikan) && $pendidikan->tingkatan == 4) ? 'selected' : '' }}">SMA/SMK</option>
-                                                <option value="5 {{ (isset($pendidikan) && $pendidikan->tingkatan == 5) ? 'selected' : '' }}">D3</option>
-                                                <option value="6 {{ (isset($pendidikan) && $pendidikan->tingkatan == 6) ? 'selected' : '' }}">S1</option>
-                                                <option value="7 {{ (isset($pendidikan) && $pendidikan->tingkatan == 7) ? 'selected' : '' }}">S2</option>
-                                                <option value="8 {{ (isset($pendidikan) && $pendidikan->tingkatan == 8) ? 'selected' : '' }}">S3</option>
-                                            </select>
-                                        </div>
+
+                                <div class="table-responsive m-t-40">
+                                    <table id="example23"
+                                        class="display nowrap table table-hover table-striped table-bordered"
+                                        cellspacing="0" width="100%">
+                                        <thead class="thead-dark">
+                                            <tr>
+                                                <th>Category</th>
+                                                <th>Nama</th>
+                                                <th>Deskripsi</th>
+                                                <th>Foto Produk</th>
+                                                <th>Tanggal Dibuat</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            <!-- 
+
+                                            <?php
+                                            $sql = "SELECT * FROM restaurant order by rs_id desc";
+                                            $query = mysqli_query($db, $sql);
+
+                                            if (!mysqli_num_rows($query) > 0) {
+                                                echo '<td colspan="11"><center>No Restaurants</center></td>';
+                                            } else {
+                                                while ($rows = mysqli_fetch_array($query)) {
+
+                                                    $mql = "SELECT * FROM res_category where c_id='" . $rows['c_id'] . "'";
+                                                    $res = mysqli_query($db, $mql);
+                                                    $row = mysqli_fetch_array($res);
+
+                                                    echo ' <tr><td>' . $row['c_name'] . '</td>
+																								<td>' . $rows['title'] . '</td>
+																								<td>' . $rows['email'] . '</td>
+																								<td>' . $rows['phone'] . '</td>
+																								<td>' . $rows['url'] . '</td>
+																								
+																								
+																								<td>' . $rows['o_hr'] . '</td>
+																								<td>' . $rows['c_hr'] . '</td>
+																								<td>' . $rows['o_days'] . '</td>
+																								
+																								<td>' . $rows['deskripsi'] . '</td>
+																								
+																								<td><div class="col-md-3 col-lg-8 m-b-10">
+																								<center><img src="Res_img/' . $rows['image'] . '" class="img-responsive radius"  style="min-width:150px;min-height:100px;"/></center>
+																								</div></td>
+																								
+																								<td>' . $rows['date'] . '</td>
+																									 <td><a href="delete_restaurant.php?res_del=' . $rows['rs_id'] . '" class="btn btn-danger btn-flat btn-addon btn-xs m-b-10"><i class="fa fa-trash-o" style="font-size:16px"></i></a> 
+																									 <a href="update_restaurant.php?res_upd=' . $rows['rs_id'] . '" class="btn btn-info btn-flat btn-addon btn-sm m-b-10 m-l-5"><i class="fa fa-edit"></i></a>
+																									</td></tr>';
+
+
+
+                                                }
+                                            }
+
+
+                                            ?> -->
+
+
+
+
+
+
+                                        </tbody>
+                                    </table>
                                 </div>
-                                <div class="form-group">
-                                    <label for="curl" class="control-label col-lg-2">Tahun Masuk <span
-                                        class="required">*</span></label>
-                                        <div class="col-lg-10">
-                                            <input id="tahun_masuk" type="text" name="tahun_masuk" class="form-control" value="{{ isset($pendidikan) ? $pendidikan->tahun_masuk : '' }}"
-                                            required>
-                                        </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="curl" class="control-label col-lg-2">Tahun Selesai <span
-                                        class="required">*</span></label>
-                                        <div class="col-lg-10">
-                                            <input id="tahun_keluar" type="text" name="tahun_keluar" class="form-control" value="{{ isset($pendidikan) ? $pendidikan->tahun_keluar : '' }}"
-                                            required>
-                                        </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-lg-offset-2 col-lg-10">
-                                        <button class="btn btn-primary" type="submit">Save</button>
-                                        <a href="{{ route('pendidikan.index') }}"><button class="btn btn-default" type="button">Cancel</button></a>
-                                    </div>
-                                </div>
-                            </form>
                             </div>
                         </div>
-                    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    </div>
+
                 </div>
             </div>
-        </section>
-    </section>
-@endsection
-@push('content-css')
-<link rel="stylesheet" href="{{ asset('backend/css/bootstrap-datepicker.css') }}">
-@endpush
-@push('content-js')
-<script src="{{ asset('backend/js/bootstrap-datepicker.js') }}"></script>
-<script type="text/javascript">
-  $('#tahun_masuk').datepicker({
-    format: "yyyy",
-    viewMode: "years",
-    minViewMode: "years"
-  });
-  $('#tahun_keluar').datepicker({
-    format: "yyyy",
-    viewMode: "years",
-    minViewMode: "years"
-  });
-</script>
-@endpush
+        </div>
+    </div>
+
+    </div>
+
+    <footer class="footer"> © 2023 - Owri Cloth </footer>
+
+    </div>
+
+    </div>
+
+    <script src="{{ asset('backend/js/lib/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('backend/js/lib/bootstrap/js/popper.min.js')}}"></script>
+    <script src="{{ asset('backend/js/lib/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('backend/js/jquery.slimscroll.js')}}"></script>
+    <script src="{ asset('backend/js/sidebarmenu.js')}}"></script>
+    <script src="{{ asset('backend/js/lib/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
+    <script src="{{ asset('backend/js/custom.min.js')}}"></script>
+
+    <script src="{{ asset('backend/js/lib/datatables/datatables.min.js')}}"></script>
+    <script src="{{ asset('backend/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js')}}">
+    </script>
+    <script src="{{ asset('backend/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js')}}">
+    </script>
+    <script src="{{ asset('backend/js/lib/datatables/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js')}}">
+    </script>
+    <script src="{{ asset('backend/js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js')}}">
+    </script>
+    <script src="{{ asset('backend/js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js')}}">
+    </script>
+    <script src="{{ asset('backend/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js')}}">
+    </script>
+    <script src="{{ asset('backend/js/lib/datatables/datatables-init.js')}}"></script>
+</body>
+
+</html>
